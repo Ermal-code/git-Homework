5fbec4ec7d195fcf12fd487334842a191fecc2c0
Ermal-code <72197798+Ermal-code@users.noreply.github.com>
2020-11-08 14:34:29 +0100

    made all Fetch with one function

--- module-3/D3/M3D3script.js
+++ module-3/D3/M3D3script.js
@@ -1,6 +1,6 @@
-const loadImg = function () {
+const loadImg = function (str) {
   let myToast = document.querySelector(".toast");
-  fetch("http://www.splashbase.co/api/v1/images/search?query=green")
+  fetch("http://www.splashbase.co/api/v1/images/search?query=" + str)
     .then((response) => response.json())
     .then((imgs) => {
       console.log(imgs);
@@ -11,8 +11,18 @@ const loadImg = function () {
       let sum = urlS.map((urls) => urls.id).reduce((sum, id) => sum + id, 0);
 
       let toastBody = document.querySelector(".toast-body");
-      toastBody.innerHTML =
-        `Total images loaded: ${urlS.length}` + "<br>Sum of Id is : " + sum;
+      if (urlS.length === 0) {
+        toastBody.style.backgroundColor = "tomato";
+        toastBody.style.color = "white";
+        myToast.style.top = "30%";
+        myToast.style.right = "45%";
+        toastBody.innerHTML = "There is no such thing in our database";
+      } else {
+        toastBody.innerHTML =
+          `Total images loaded: ${urlS.length}` + "<br>Sum of Id is : " + sum;
+        myToast.style.top = "20%";
+        myToast.style.right = "0";
+      }
       myToast.classList.add("show");
 
       urlArray = imgs.images.map((urls) => urls.url);
@@ -102,7 +112,7 @@ primarybtn.onclick = function () {
       .querySelectorAll(".row")[1]
       .removeChild(document.querySelectorAll(".row")[1].firstChild);
   }
-  loadImg();
+  loadImg("green");
   console.log(urlArray);
 };
 
@@ -113,103 +123,6 @@ primarybtn.onclick = function () {
 //4) The Edit button should be replace with a "Hide" button.
 //5) When the hide button is pressed, the whole picture card disappears.
 
-const loadSecImg = function () {
-  let myToast = document.querySelector(".toast");
-  fetch("http://www.splashbase.co/api/v1/images/search?query=blue")
-    .then((response) => response.json())
-    .then((imgs) => {
-      console.log(imgs);
-      urlArray = imgs.images.map((urls) => urls.url);
-      let urlS =
-        imgs.images.length <= 9 ? imgs.images : imgs.images.slice(0, 9);
-
-      let sum = urlS.map((urls) => urls.id).reduce((sum, id) => sum + id, 0);
-
-      let toastBody = document.querySelector(".toast-body");
-      toastBody.innerHTML =
-        `Total images loaded: ${urlS.length}` + "<br>Sum of Id is : " + sum;
-      myToast.classList.add("show");
-
-      console.log(sum);
-
-      for (let i = 0; i < urlS.length; i++) {
-        let divElement = document.createElement("div");
-        divElement.classList.add("col-md-4");
-
-        document.querySelectorAll(".row")[1].appendChild(divElement);
-        let col = document.querySelectorAll(".col-md-4");
-        col[i].innerHTML = `
-                                <div class="card mb-4 shadow-sm">
-                                <img class="img-fluid" style="height:200px" src="${urlS[i].url}">
-                                <div class="card-body">
-                                    <p class="card-text">
-                                        This is a wider card with supporting text below as a natural
-                                        lead-in to additional content. This content is a little bit
-                                        longer.
-                                    </p>
-                                    <div class="d-flex justify-content-between align-items-center">
-                                        <div class="btn-group">
-                                            <button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target="#mod${urlS[i].id}">
-                                            
-                                                View
-                                            </button>
-                                            <button type="button" class="btn btn-sm btn-outline-secondary">
-                                                Hide 
-                                            </button>
-                                        </div>
-                                        <small class="text-danger">${urlS[i].id}</small>
-                                    </div>
-                                </div>
-                                <div class="modal fade" id="mod${urlS[i].id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <img class ="img-fluid" style="height:200px" src="${urlS[i].url}">
-                                    </div>
-                                    <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                    </div>
-                                    </div>
-                                </div>
-                                </div>
-                                </div>
-                                `;
-        const hideIMG = (e) => {
-          const node = e.currentTarget;
-
-          const firstParent = node.parentElement;
-          const secondParent = firstParent.parentElement;
-          const thirdParent = secondParent.parentElement;
-          const fourthParent = thirdParent.parentElement;
-
-          let imgCard = fourthParent.querySelector(".card > :first-child");
-
-          if (imgCard.style.display === "none") {
-            imgCard.style.display = "flex";
-          } else {
-            imgCard.style.display = "none";
-          }
-          console.log(node);
-        };
-
-        let hideBtn = document.querySelectorAll(".card .btn-sm:last-of-type");
-
-        for (let i = 0; i < hideBtn.length; i++) {
-          hideBtn[i].onclick = hideIMG;
-        }
-      }
-    });
-  setTimeout(function () {
-    myToast.classList.remove("show");
-  }, 5000);
-};
-
 let secbtn = document.querySelector(".btn-secondary");
 secbtn.onclick = function () {
   while (document.querySelectorAll(".row")[1].firstChild) {
@@ -217,118 +130,8 @@ secbtn.onclick = function () {
       .querySelectorAll(".row")[1]
       .removeChild(document.querySelectorAll(".row")[1].firstChild);
   }
-  loadSecImg();
+  loadImg("blue");
   console.log(urlArray);
-};
-
-const loadSearchImg = function () {
-  let myToast = document.querySelector(".toast");
-  let urlFetch =
-    "http://www.splashbase.co/api/v1/images/search?query=" +
-    document.querySelector(".jumbotron input[type='text']").value;
-  fetch(urlFetch)
-    .then((response) => response.json())
-    .then((imgs) => {
-      console.log(imgs);
-      urlArray = imgs.images.map((urls) => urls.url);
-
-      let urlS =
-        imgs.images.length <= 9 ? imgs.images : imgs.images.slice(0, 9);
-
-      let sum = urlS.map((urls) => urls.id).reduce((sum, id) => sum + id, 0);
-
-      let toastBody = document.querySelector(".toast-body");
-      if (urlS.length === 0) {
-        toastBody.style.backgroundColor = "tomato";
-        toastBody.style.color = "white";
-        myToast.style.top = "50%";
-        myToast.style.right = "45%";
-        toastBody.innerHTML = "There is no such thing in our database";
-      } else {
-        toastBody.innerHTML =
-          `Total images loaded: ${urlS.length}` + "<br>Sum of Id is : " + sum;
-        myToast.style.top = "20%";
-        myToast.style.right = "0";
-      }
-      myToast.classList.add("show");
-
-      console.log(sum);
-
-      for (let i = 0; i < urlS.length; i++) {
-        let divElement = document.createElement("div");
-        divElement.classList.add("col-md-4");
-
-        document.querySelectorAll(".row")[1].appendChild(divElement);
-        let col = document.querySelectorAll(".col-md-4");
-        col[i].innerHTML = `
-                                  <div class="card mb-4 shadow-sm">
-                                  <img class="img-fluid" style="height:200px" src="${urlS[i].url}">
-                                  <div class="card-body">
-                                      <p class="card-text">
-                                          This is a wider card with supporting text below as a natural
-                                          lead-in to additional content. This content is a little bit
-                                          longer.
-                                      </p>
-                                      <div class="d-flex justify-content-between align-items-center">
-                                          <div class="btn-group">
-                                              <button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target="#mod${urlS[i].id}">
-                                              
-                                                  View
-                                              </button>
-                                              <button type="button" class="btn btn-sm btn-outline-secondary">
-                                                  Hide 
-                                              </button>
-                                          </div>
-                                          <small class="text-danger">${urlS[i].id}</small>
-                                      </div>
-                                  </div>
-                                  <div class="modal fade" id="mod${urlS[i].id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                  <div class="modal-dialog">
-                                      <div class="modal-content">
-                                      <div class="modal-header">
-                                          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                          <span aria-hidden="true">&times;</span>
-                                          </button>
-                                      </div>
-                                      <div class="modal-body">
-                                          <img class ="img-fluid" style="height:200px" src="${urlS[i].url}">
-                                      </div>
-                                      <div class="modal-footer">
-                                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                      </div>
-                                      </div>
-                                  </div>
-                                  </div>
-                                  </div>`;
-        const hideIMG = (e) => {
-          const node = e.currentTarget;
-
-          const firstParent = node.parentElement;
-          const secondParent = firstParent.parentElement;
-          const thirdParent = secondParent.parentElement;
-          const fourthParent = thirdParent.parentElement;
-
-          let imgCard = fourthParent.querySelector(".card > :first-child");
-
-          if (imgCard.style.display === "none") {
-            imgCard.style.display = "flex";
-          } else {
-            imgCard.style.display = "none";
-          }
-          console.log(node);
-        };
-
-        let hideBtn = document.querySelectorAll(".card .btn-sm:last-of-type");
-
-        for (let i = 0; i < hideBtn.length; i++) {
-          hideBtn[i].onclick = hideIMG;
-        }
-      }
-    });
-  setTimeout(function () {
-    myToast.classList.remove("show");
-  }, 5000);
 };
 
 let searchBtn = document.querySelector(".jumbotron #button-addon1");
@@ -340,7 +143,7 @@ searchBtn.onclick = function () {
       .removeChild(document.querySelectorAll(".row")[1].firstChild);
   }
 
-  loadSearchImg();
+  loadImg(document.querySelector(".jumbotron input[type='text']").value);
   console.log(urlArray);
 };
 
