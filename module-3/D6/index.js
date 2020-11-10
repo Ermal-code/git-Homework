9e0d119065f4860e520f72692e4fc7903f8e9813
Ermal-code <72197798+Ermal-code@users.noreply.github.com>
2020-11-10 23:02:24 +0100

    Ex 4 done!

--- module-3/D6/index.js
+++ module-3/D6/index.js
@@ -1,16 +1,31 @@
+let drowpdownLinks = document.querySelectorAll(".dropdown-menu a");
+let actionBtn = document.querySelector(".input-group-prepend .btn");
+let filterInput = document.querySelector(".input-group input[type='text']");
+filterInput.placeholder = "Filter by name";
+
+for (let i = 0; i < drowpdownLinks.length; i++) {
+  drowpdownLinks[i].onclick = () => {
+    actionBtn.innerText = drowpdownLinks[i].innerText;
+    filterInput.placeholder =
+      "Filter by " + drowpdownLinks[i].innerText.toLowerCase();
+  };
+}
+
 const userCard = (userInfo) => {
   return `<div class="card mb-3">
-    <div class="card-header" id="card1">
-      Name : ${userInfo.name}
+    <div class="card-header text-info" >
+      Name: ${userInfo.name}
     </div>
-    <div class="card-header" id="card2" style="display:none">
-      Username : ${userInfo.username}
+    <div class="card-header text-danger"  >
+      Username: ${userInfo.username}
     </div>
-    <div class="card-header" id="card3" style="display:none">
-      Email : ${userInfo.email}
+    <div class="card-header text-success"  >
+      Email: ${userInfo.email}
     </div>
     <ul class="list-group list-group-flush">
-      <li class="list-group-item">Adress: ${userInfo.address.street},${userInfo.address.suite},${userInfo.address.city}(${userInfo.address.zipcode})</li>
+      <li class="list-group-item">Adress: ${Object.values(
+        userInfo.address
+      ).join(" ")}</li>
       <li class="list-group-item">Phone: ${userInfo.phone}</li>
       <li class="list-group-item">Company: ${userInfo.company.name}</li>
       <li class="list-group-item">Website: ${userInfo.website}</li>
@@ -21,7 +36,7 @@ const userCard = (userInfo) => {
 let query = "";
 
 const handleSearchQuery = (event) => {
-  query = event.target.value;
+  query = event.target.value.toLowerCase();
 };
 
 const fetchUsers = async () => {
@@ -30,71 +45,56 @@ const fetchUsers = async () => {
 
     let users = await response.json();
 
+    nameArray(users);
+
     let row = document.querySelector(".row");
     let user = [];
 
     if (query !== "") {
-      user = users.filter((res) => res.name === query);
+      if (actionBtn.innerText === "Name") {
+        user = users.filter((res) => res.name.toLowerCase().includes(query));
+      } else if (actionBtn.innerText === "Username") {
+        user = users.filter((res) =>
+          res.username.toLowerCase().includes(query)
+        );
+      } else {
+        user = users.filter((res) => res.email.toLowerCase().includes(query));
+      }
     } else {
       user = [...users];
     }
-    console.log(user);
 
     row.innerHTML = "";
+
     user.forEach((element) => {
+      delete element.address.geo;
       let col = document.createElement("div");
       col.classList.add("col-12", "col-md-6", "col-lg-4");
 
       col.innerHTML = userCard(element);
 
       row.appendChild(col);
+      addressArray(element);
     });
   } catch {
     (err) => console.log(err);
   }
 };
 
-let drowpdownLinks = document.querySelectorAll(".dropdown-menu a");
-
-const card = function () {
-  let cardheader = document.querySelectorAll("#card1");
-  let cardheader1 = document.querySelectorAll("#card2");
-  let cardheader2 = document.querySelectorAll("#card3");
-  for (let i = 0; i < cardheader.length; i++) {
-    if (cardheader[i].style.display === "none") {
-      cardheader[i].style.display = "block";
-      cardheader1[i].style.display = "none";
-      cardheader2[i].style.display = "none";
-    }
-  }
-};
-
-const card1 = function () {
-  let cardheader = document.querySelectorAll("#card1");
-  let cardheader1 = document.querySelectorAll("#card2");
-  let cardheader2 = document.querySelectorAll("#card3");
-  for (let i = 0; i < cardheader1.length; i++) {
-    if (cardheader1[i].style.display === "none") {
-      cardheader1[i].style.display = "block";
-      cardheader[i].style.display = "none";
-      cardheader2[i].style.display = "none";
-    }
-  }
-};
-
-const card2 = function () {
-  let cardheader = document.querySelectorAll("#card1");
-  let cardheader1 = document.querySelectorAll("#card2");
-  let cardheader2 = document.querySelectorAll("#card3");
-  for (let i = 0; i < cardheader2.length; i++) {
-    if (cardheader1[i].style.display === "none") {
-      cardheader2[i].style.display = "block";
-      cardheader1[i].style.display = "none";
-      cardheader[i].style.display = "none";
-    }
-  }
-};
-
 window.onload = () => {
   fetchUsers();
 };
+
+const addressArray = (address) => {
+  delete address.address.geo;
+  let addressEl = Object.values(address.address).join(", ");
+
+  addressArr.push(addressEl);
+};
+
+const nameArray = (names) => {
+  nameArr = names.map((res) => res.name);
+};
+
+let addressArr = [];
+let nameArr = [];
