f74113678db69ef08a86b7761f4e62285bb43f27
Ermal-code <72197798+Ermal-code@users.noreply.github.com>
2020-11-19 17:11:57 +0100

    removed badge and warning sing

--- module-4/D4/src/App.jsx
+++ module-4/D4/src/App.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import MyBadge from "./Components/MyBadge";
-import WarningSign from "./Components/WarningSign";
 import fantasy from "./data/fantasy.json";
 import BookList from "./Components/BookList";
 
@@ -10,8 +8,6 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <WarningSign text="Exercise one " />
-        <MyBadge text="Exercise two" color="primary" />
         <BookList objArr={fantasy} />
       </div>
     );
