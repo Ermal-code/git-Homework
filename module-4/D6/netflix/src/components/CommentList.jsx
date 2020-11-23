695213447a5f378dcc2fb368cfe044469075c8b9
Ermal-code <72197798+Ermal-code@users.noreply.github.com>
2020-11-23 15:40:45 +0100

    Update CommentList.jsx

--- module-4/D6/netflix/src/components/CommentList.jsx
+++ module-4/D6/netflix/src/components/CommentList.jsx
@@ -9,7 +9,8 @@ class CommentList extends React.Component {
   fetchComments = async () => {
     try {
       let response = await fetch(
-        "https://striveschool-api.herokuapp.com/api/comments/",
+        "https://striveschool-api.herokuapp.com/api/comments/" +
+          this.props.movieId,
 
         {
           headers: new Headers({
@@ -31,7 +32,7 @@ class CommentList extends React.Component {
     this.fetchComments();
   };
 
-  componentDidUpdate = (previousState) => {
+  componentDidUpdate = (previousProps, previousState) => {
     if (previousState !== this.state.comments) {
       this.fetchComments();
     }
@@ -48,40 +49,38 @@ class CommentList extends React.Component {
           />
         ) : (
           <>
-            {this.state.comments
-              .filter((res) => res.elementId === this.props.movieId)
-              .map((comment, index) => {
-                let variant = "";
+            {this.state.comments.map((comment, index) => {
+              let variant = "";
 
-                switch (comment.rate) {
-                  case 1:
-                    variant = "danger";
-                    break;
-                  case 2:
-                    variant = "warning";
-                    break;
-                  case 3:
-                    variant = "secondary";
-                    break;
-                  default:
-                    variant = "success";
-                    break;
-                }
-                return (
-                  <ListGroup key={index}>
-                    <ListGroup.Item className="text-dark">
-                      Comment: {comment.comment}
-                    </ListGroup.Item>
-                    <ListGroup.Item className="text-dark">
-                      <span>Rate </span>
-                      <Badge pill variant={variant}>
-                        {comment.rate}
-                      </Badge>
-                    </ListGroup.Item>
-                    <ListGroup.Item></ListGroup.Item>
-                  </ListGroup>
-                );
-              })}
+              switch (comment.rate) {
+                case 1:
+                  variant = "danger";
+                  break;
+                case 2:
+                  variant = "warning";
+                  break;
+                case 3:
+                  variant = "secondary";
+                  break;
+                default:
+                  variant = "success";
+                  break;
+              }
+              return (
+                <ListGroup key={index}>
+                  <ListGroup.Item className="text-dark">
+                    Comment: {comment.comment}
+                  </ListGroup.Item>
+                  <ListGroup.Item className="text-dark">
+                    <span>Rate </span>
+                    <Badge pill variant={variant}>
+                      {comment.rate}
+                    </Badge>
+                  </ListGroup.Item>
+                  <ListGroup.Item></ListGroup.Item>
+                </ListGroup>
+              );
+            })}
           </>
         )}
       </div>
