61129d7b59af861067046b64fed40ac9fc641ecf
Ermal-code <72197798+Ermal-code@users.noreply.github.com>
2020-12-09 16:58:39 +0100

    almost finished validation

--- module-5/D3/serverNode/src/server.js
+++ module-5/D3/serverNode/src/server.js
@@ -3,6 +3,14 @@ const express = require("express");
 const cors = require("cors");
 
 const students = require("./services/students");
+const projects = require("./services/projects");
+const {
+  forbiddenErrorHandler,
+  notFoundErrorHandler,
+  badRequestErrorHandler,
+  unauthorizedErrorHandler,
+  catchAllErrorHandler,
+} = require("./errorHandling");
 
 const server = express();
 
@@ -10,6 +18,13 @@ server.use(cors());
 server.use(express.json());
 
 server.use("/students", students);
+server.use("/projects", projects);
+
+server.use(badRequestErrorHandler);
+server.use(notFoundErrorHandler);
+server.use(forbiddenErrorHandler);
+server.use(unauthorizedErrorHandler);
+server.use(catchAllErrorHandler);
 
 server.listen(process.env.PORT, () =>
   console.log("Server running on port 3003")
---
+++ module-5/D3/serverNode/src/services/projects/index.js
@@ -0,0 +1,185 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const uniqid = require("uniqid");
+const { check, validationResult } = require("express-validator");
+
+const router = express.Router();
+
+const fileRead = (fileName) => {
+  const buffer = fs.readFileSync(path.join(__dirname, fileName));
+  const fileContent = buffer.toString();
+  return JSON.parse(fileContent);
+};
+
+router.get("/", (req, res, next) => {
+  try {
+    const projectDb = fileRead("projects.json");
+    res.status(200).send(projectDb);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+router.get("/:id", (req, res, next) => {
+  try {
+    const projectDb = fileRead("projects.json");
+    const filteredData = projectDb.find(
+      (project) => project.ID === req.params.id
+    );
+
+    if (!filteredData) {
+      const err = new Error();
+      err.message = "ID is incorrect";
+      err.httpStatusCode = 404;
+      next(err);
+    } else {
+      res.status(200).send(filteredData);
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+router.post(
+  "/",
+  [
+    check("name")
+      .exists()
+      .withMessage("Name is required")
+      .isLength({ min: 3 })
+      .withMessage("Name must contain more than 3 characters"),
+    check("studentID").exists().withMessage("Student ID is required"),
+    check("description")
+      .exists()
+      .withMessage("Description is required")
+      .isLength({ min: 8, max: 50 })
+      .withMessage(
+        "Description should be longer than 8 charcacters and shorter than 50 characters"
+      ),
+    check("repoURL").exists().withMessage("Repo Url of project is required"),
+  ],
+  (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+
+      if (errors.isEmpty()) {
+        const projectDb = fileRead("projects.json");
+        const newProject = {
+          ...req.body,
+          ID: uniqid(),
+          creationDate: new Date(),
+        };
+
+        projectDb.push(newProject);
+
+        fs.writeFileSync(
+          path.join(__dirname, "projects.json"),
+          JSON.stringify(projectDb)
+        );
+
+        res.status(201).send({ ID: newProject.ID });
+      } else {
+        const err = new Error();
+        err.message = errors;
+        err.httpStatusCode = 400;
+        next(err);
+      }
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+);
+
+router.put(
+  "/:id",
+  [
+    check("name")
+      .exists()
+      .withMessage("Name is required")
+      .isLength({ min: 3 })
+      .withMessage("Name must contain more than 3 characters"),
+    check("studentID").exists().withMessage("Student ID is required"),
+    check("description")
+      .exists()
+      .withMessage("Description is required")
+      .isLength({ min: 8, max: 50 })
+      .withMessage(
+        "Description should be longer than 8 charcacters and shorter than 50 characters"
+      ),
+    check("repoURL").exists().withMessage("Repo Url of project is required"),
+  ],
+  (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+
+      if (errors.isEmpty()) {
+        const modifiedPoject = req.body;
+        const projectDb = fileRead("projects.json");
+        const modifiedDb = projectDb.filter(
+          (project) => project.ID !== req.params.id
+        );
+        const findID = projectDb.find(
+          (project) => project.ID === req.params.id
+        );
+
+        if (!findID) {
+          const err = new Error();
+          err.message = "ID is incorrect";
+          err.httpStatusCode = 404;
+          next(err);
+        } else {
+          modifiedPoject.ID = req.params.id;
+          modifiedDb.push(modifiedPoject);
+
+          fs.writeFileSync(
+            path.join(__dirname, "projects.json"),
+            JSON.stringify(modifiedDb)
+          );
+
+          res.status(200).send(modifiedDb);
+        }
+      } else {
+        const err = new Error();
+        err.message = errors;
+        err.httpStatusCode = 400;
+        next(err);
+      }
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+);
+
+router.delete("/:id", (req, res, next) => {
+  try {
+    const projectDb = fileRead("projects.json");
+    const modifiedDb = projectDb.filter(
+      (project) => project.ID !== req.params.id
+    );
+
+    const findID = projectDb.find((project) => project.ID === req.params.id);
+    if (!findID) {
+      const err = new Error();
+      err.message = "ID is incorrect";
+      err.httpStatusCode = 404;
+      next(err);
+    } else {
+      fs.writeFileSync(
+        path.join(__dirname, "projects.json"),
+        JSON.stringify(modifiedDb)
+      );
+
+      res.status(204).send();
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+module.exports = router;
